546cbb578c7e8044cd1980776ca28720411d2d39
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-04-18 10:37:07 +0530

    Add loging page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import NavBarTop from "./componenet/navbar/Navbar";
 import ClippedDrawer from "./componenet/navbar/nav";
+import ResponsiveDrawer from "./componenet/navbar/nav";
 
 function App() {
   return (
    <div>
 
-     <ClippedDrawer/>
+     <ResponsiveDrawer/>
    </div>
   );
 }
