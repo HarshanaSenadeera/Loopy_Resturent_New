6a4c819c89b8e1ca5464c42377edea1e083b3e75
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-04-18 12:34:39 +0530

    crate card component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import NavBarTop from "./componenet/navbar/Navbar";
 import ClippedDrawer from "./componenet/navbar/nav";
 import ResponsiveDrawer from "./componenet/navbar/nav";
+import ImgMediaCard from "./componenet/dishes/dishes";
 
 function App() {
   return (
    <div>
 
-     <ResponsiveDrawer/>
+     <ImgMediaCard/>
    </div>
   );
 }
