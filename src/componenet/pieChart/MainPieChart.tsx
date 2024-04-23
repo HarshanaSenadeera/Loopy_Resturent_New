0e56a69c1edf4c93f7861d458b0f30715da4c3bd
nimeshaMadubashini <nimeshamadubashini040@gmail.com>
2024-04-23 19:43:04 +0530

    card update

--- src/componenet/pieChart/MainPieChart.tsx
+++ src/componenet/pieChart/MainPieChart.tsx
@@ -3,18 +3,30 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import BasicPie from "./FoodChart";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
 
 export const MainPieChart = () => {
     return (
         <Box sx={{ width: '100%' }}>
-            <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                <Grid item xs={6}>
-                    <BasicPie/>
+            <Grid container rowSpacing={1}  columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                <Grid item   xs={6}>
+                    <Paper elevation={3} sx={{ p: 2 }}> {/* Add shadow with elevation and padding */}
+                        <Typography variant="h4"  component="div">
+                            Mac Miller
+                        </Typography>
+                        <BasicPie />
+                    </Paper>
                 </Grid>
-                <Grid item xs={6}>
-                    <BasicPie/>
+                <Grid item  xs={6}>
+                    <Paper elevation={3} sx={{ p: 2 }}> {/* Add shadow with elevation and padding */}
+                        <Typography variant="h4"   component="div" sx={{fontStyle:"bold"}}>
+                            Mac Miller
+                        </Typography>
+                        <BasicPie />
+                    </Paper>
                 </Grid>
-
             </Grid>
-        </Box>    );
+        </Box>
+    );
 };
