2dea38589fc9cd6518020d712a80e0c5f4e03b35
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-05-03 12:39:59 +0530

    Update report

--- src/Common/MainReports/Report.tsx
+++ src/Common/MainReports/Report.tsx
@@ -1,3 +1,4 @@
+/*
 import * as React from 'react';
 import {DailyIncome} from "../Reports/DailyIncome";
 import {MounthlyIncome} from "../Reports/MounthlyIncome";
@@ -7,7 +8,7 @@ import Grid from "@mui/material/Grid";
 
 export default function ReportMain(): React.JSX.Element {
     return (
-        <Grid container spacing={1}>
+        <Grid container spacing={2}> {/!* Adjust spacing value as needed *!/}
             <Grid lg={4} sm={6} xs={12}>
                 <DailyIncome diff={16} trend="down" sx={{ height: '100%' }} value="Rs:00000.00" />
             </Grid>
@@ -18,5 +19,42 @@ export default function ReportMain(): React.JSX.Element {
                 <Annually diff={16} trend="down" sx={{ height: '100%' }} value="Rs:00000.00" />
             </Grid>
         </Grid>
+    );
+}
+*/
+
+
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import {DailyIncome} from "../Reports/DailyIncome";
+import {MounthlyIncome} from "../Reports/MounthlyIncome";
+import {Annually} from "../Reports/Annually";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+export default function ReportMain() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <DailyIncome diff={16} trend="down" sx={{ height: '100%' }} value="Rs:00000.00" />
+                </Grid>
+                <Grid item xs>
+                    <MounthlyIncome diff={16} trend="up" sx={{ height: '100%' }} value="Rs:00000.00" />
+                </Grid>
+                <Grid item xs>
+                    <Annually diff={16} trend="down" sx={{ height: '100%' }} value="Rs:00000.00" />
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
