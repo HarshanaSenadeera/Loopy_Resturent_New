58be619e03ebe213252e023acb76c613dc7d3ca1
Harshana Senadeera <116424581+HarshanaSenadeera@users.noreply.github.com>
2024-05-13 11:01:46 +0530

    add view details button

--- src/Common/Page/Settings/Setting.tsx
+++ src/Common/Page/Settings/Setting.tsx
@@ -160,7 +160,10 @@ export default function SettingMain() {
                                             <TableCell>{buyer.type}</TableCell>
                                             <TableCell>{buyer.type === 'KOT' ? buyer.item : buyer.item2}</TableCell>
                                             <TableCell>
-                                                <Button variant="outlined" color="error" sx={{ borderRadius: '20px' }} onClick={handleOpen1}>pending</Button>
+                                                <Button variant="outlined" color="error" sx={{ borderRadius: '20px' }} >pending</Button>
+                                            </TableCell>
+                                            <TableCell>
+                                                <Button variant="outlined" color="error" sx={{ borderRadius: '20px' }} onClick={handleOpen1}>View Details</Button>
                                             </TableCell>
                                         </TableRow>
 
